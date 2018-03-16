5bfc04d4c109da7e7d7a891cc388e89b61dbde93
Petar  Anastasov <anastasov.5ar@gmail.com>
2018-03-16 15:19:20 +0100

    modified humanize function

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -6,7 +6,7 @@ class Feature < ApplicationRecord
 
   def humanize
     durr = duration
-    [[60, :seconds], [60, :minutes], [24, :hours], [1000, :days]].map{ |count, name|
+    [[60, :seconds], [60, :minutes], [24, :hours], [30, :days], [1000, :months]].map{ |count, name|
       if durr > 0
         durr, n = durr.divmod(count)
         "#{n.to_i} #{name}"
