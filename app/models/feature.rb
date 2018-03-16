732cb7ae99284cf45901933c212b446785bbba50
Dunya Kirkali <dunya.kirkali@icemobile.com>
2018-03-16 11:46:45 +0100

    oups

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -2,9 +2,10 @@ class Feature < ApplicationRecord
   belongs_to :team
 
   def humanize
+    durr = duration
     [[60, :seconds], [60, :minutes], [24, :hours], [1000, :days]].map{ |count, name|
-      if duration > 0
-        duration, n = duration.divmod(count)
+      if durr > 0
+        durr, n = durr.divmod(count)
         "#{n.to_i} #{name}"
       end
     }.compact.reverse.join(' ')
